c7f0c3b27ec8e8e675e18feb75aa9fed25a2665a
henriquefantoni <henriquegfantoni@gmail.com>
2024-06-29 19:44:03 -0300

    gifs nos icones ao passar o mouse

--- main.js
+++ main.js
@@ -12,8 +12,47 @@ setInterval(()=> {
 
     if(index<2) {
         index++
-
+        
     }else {
         index = 0
     }
 },5000)
+
+window.onload = function ()  {
+
+    var Questions = document.getElementById('questions')
+
+
+    Questions.onmouseenter = function () {
+        Questions.querySelector('img').src = "./questionsGif.gif"
+
+    }
+    Questions.onmouseleave = function () {
+        Questions.querySelector('img').src = "./assets/questions.png"
+
+    }
+
+    var grapples = document.getElementById('grapples')
+
+
+    grapples.onmouseenter = function () {
+        grapples.querySelector('img').src = "./gifGraplles.gif"
+
+    }
+    grapples.onmouseleave = function () {
+        grapples.querySelector('img').src = "./assets/logo grapples.png"
+
+    }
+
+    var boss = document.getElementById('boss')
+
+
+    boss.onmouseenter = function () {
+        boss.querySelector('img').src = "./BossGif.gif"
+
+    }
+    boss.onmouseleave = function () {
+        boss.querySelector('img').src = "./assets/BossHuntlogo.png"
+
+    }
+}
