60b1f4afcd77787e306e07e2ecb8044e3c39d295
henriquefantoni <henriquegfantoni@gmail.com>
2024-07-16 09:18:08 -0300

    criado o sobre mim

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const gifs = ["./gifGRAPLLES.gif","./BossGif.gif","./questionsGif.gif"]
 
 var index=0
 
+
+
 setInterval(()=> {
     document.getElementById("gameName").innerHTML = names[index]
     document.getElementById("gameDesc").innerHTML = descs[index]
@@ -20,6 +22,17 @@ setInterval(()=> {
 
 window.onload = function ()  {
 
+    document.getElementById("gameName").innerHTML = names[index]
+        document.getElementById("gameDesc").innerHTML = descs[index]
+        document.getElementById("heroImage").style.backgroundImage =
+        `linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.5)), url(${gifs[index]})`
+    
+        if(index<2) {
+            index++
+            
+        }else {
+            index = 0
+        }
     var Questions = document.getElementById('questions')
 
 
